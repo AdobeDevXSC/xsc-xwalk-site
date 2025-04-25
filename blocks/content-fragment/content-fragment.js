609d8b79ed60611fb6f984c5fcc6423df4aff86e
deck-adobe <deckreyes@adobe.com>
2025-04-25 11:28:02 -0700

    adding aem.page to CF block check

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -3,7 +3,7 @@
 const AEM_HOST = checkDomain()
 
 function checkDomain(){
-  if (window.location.hostname.includes("hlx.page") || window.location.hostname.includes("localhost")){
+  if (window.location.hostname.includes("hlx.page") || window.location.hostname.includes("aem.page") || window.location.hostname.includes("localhost")){
     return "https://publish-p131639-e1282833.adobeaemcloud.com/"    
   }else{
     return window.location.origin 
